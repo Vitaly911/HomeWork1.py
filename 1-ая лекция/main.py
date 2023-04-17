173b9444e7703dd3a911256e1603bc6ebf924769
Vitaly <sinicin1313@gmail.com>
2023-04-17 19:07:08 +0300

    first commit

---
+++ 1-ая лекция/main.py
@@ -0,0 +1,14 @@
+""""
+print(5)
+n = 5
+print(n)
+"""
+name = input("Введите ваше имя: ")
+if name == "Маша":
+     print("Ура, это же МАША!")
+elif name == "Марина":
+     print ("Я так ждал Вас, Марина!")
+elif name == "Ильнар":
+     print("Ильнар - топ)")
+else:
+     print("Привет, ", name)
